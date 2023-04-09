11e28e1d1636104c5e6ce04888a15a4271fe198d
ShuMAhd <shuman@zykaika.ru>
2023-04-09 14:24:40 +0800

    Вызовов исключения UnsupportedOperationException в пустойЛист

--- lectures/Lesson_2ExceptionsAndTheirHandling/AppLication.java
+++ lectures/Lesson_2ExceptionsAndTheirHandling/AppLication.java
@@ -1,18 +1,20 @@
 package lectures.Lesson_2ExceptionsAndTheirHandling;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 public class AppLication {
 
   /**
    * Специально создаём разные ошибки для вызова исключений
-   *
    * @param args
    */
   public static void main(String[] args) {
-    ПустойОбъект(); //Вызываем исключение NullPointerException
-    //ТипКлассаДругой(); //Вызываем исключение ClassCastException
-    //СтрокаКчислу();
+    //пустойОбъект(); //Вызываем исключение NullPointerException
+    //типКлассаДругой(); //Вызываем исключение ClassCastException
+    //строкаКчислу(); //Вызываем NumberFormatException
+    пустойЛист(); //Вызываем UnsupportedOperationException
   }
 
   /**
@@ -20,7 +22,7 @@ public class AppLication {
    * NullPointerException s-это исключения, возникающие при попытке использовать ссылку, которая не
    * указывает ни на какое место в памяти (null)
    */
-  static void ПустойОбъект() {
+  static void пустойОбъект() {
     String имя = null;
     System.out.println(имя.length());
   }
@@ -29,7 +31,7 @@ public class AppLication {
    * Преобразуем объект типа string к объекту типа file ClassCastException-это исключение, которое
    * может возникнуть при попытке неправильного преобразования класса из одного типа в другой.
    */
-  static void ТипКлассаДругой() {
+  static void типКлассаДругой() {
     Object object = new String("123");
     File файл = (File) object;
     System.out.println(файл);
@@ -41,9 +43,20 @@ public class AppLication {
    * 2. Пытаемся преобразовать её в число
    * NumberFormatException - неверное преобразование строки в числовой формат.
    */
-  static void СтрокаКчислу() {
+  static void строкаКчислу() {
     String номер = "123fq";
     int результат = Integer.parseInt(номер);
     System.out.println(результат);
+  }
+
+  /**
+   * Пытаемся добавить в недобовляемый список данные
+   * 1. Создаём не добовляемый список
+   * 2. Добовляем в список
+   * UnsupportedOperationException - указанной операции не существует
+   */
+  static void пустойЛист(){
+    List<Object> пустойЛист = Collections.emptyList();
+    пустойЛист.add(new Object());
   }
 }
