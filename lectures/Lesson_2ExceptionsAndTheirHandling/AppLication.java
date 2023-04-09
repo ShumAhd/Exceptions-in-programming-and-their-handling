1cbf9f4928f0336b3cf643cbd235e1234287e633
ShuMAhd <shuman@zykaika.ru>
2023-04-09 14:08:15 +0800

    Вызовов исключения NumberFormatException в СтрокаКчислу

--- lectures/Lesson_2ExceptionsAndTheirHandling/AppLication.java
+++ lectures/Lesson_2ExceptionsAndTheirHandling/AppLication.java
@@ -6,34 +6,44 @@ public class AppLication {
 
   /**
    * Специально создаём разные ошибки для вызова исключений
+   *
    * @param args
    */
   public static void main(String[] args) {
-    //NullPointerException1(); //Вызываем исключение NullPointerException
-    ClassCastException1(); //Вызываем исключение ClassCastException
+    ПустойОбъект(); //Вызываем исключение NullPointerException
+    //ТипКлассаДругой(); //Вызываем исключение ClassCastException
+    //СтрокаКчислу();
   }
 
   /**
-    Создаём объект со значением "ничего нет"
-    обращаемся к объекту и хотим узнать длину массива
-    NullPointerException s-это исключения,
-    возникающие при попытке использовать ссылку,
-    которая не указывает ни на какое место в памяти (null)
+   * Создаём объект со значением "ничего нет" обращаемся к объекту и хотим узнать длину массива
+   * NullPointerException s-это исключения, возникающие при попытке использовать ссылку, которая не
+   * указывает ни на какое место в памяти (null)
    */
-  static void NullPointerException1() {
-    String name = null;
-    System.out.println(name.length());
+  static void ПустойОбъект() {
+    String имя = null;
+    System.out.println(имя.length());
   }
 
   /**
-    Преобразуем объект типа string к объекту типа file
-    ClassCastException-это исключение,
-    которое может возникнуть при попытке неправильного преобразования
-    класса из одного типа в другой.
+   * Преобразуем объект типа string к объекту типа file ClassCastException-это исключение, которое
+   * может возникнуть при попытке неправильного преобразования класса из одного типа в другой.
    */
-  static void ClassCastException1(){
+  static void ТипКлассаДругой() {
     Object object = new String("123");
-    File file = (File) object;
-    System.out.println(file);
+    File файл = (File) object;
+    System.out.println(файл);
+  }
+
+  /**
+   * Попытка преобразовать строку к числу, вызов исключения
+   * 1. Создаём строку с числом
+   * 2. Пытаемся преобразовать её в число
+   * NumberFormatException - неверное преобразование строки в числовой формат.
+   */
+  static void СтрокаКчислу() {
+    String номер = "123fq";
+    int результат = Integer.parseInt(номер);
+    System.out.println(результат);
   }
 }
